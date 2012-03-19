502869d0a0fb5ae17531dca573b0831e69f91ef6
elmotec <jlecomte1972@yahoo.com>
2012-03-18 20:25:28 -0400

    Added MIT license. Added command line tests and fixes. Added logging

--- massedit.py
+++ massedit.py
@@ -10,14 +10,21 @@ http://code.activestate.com/recipes/\
 
 The core was fleshed up a little, and here we are. If you find it usefull and
 enhance it please, do not forget to submit patches. Thanks!
+
+Licensed under the term of MIT License. See file LICENSE.
 """
 
 __version__ = '$Revision$'
 
+import os
 import logging
 import argparse
 import difflib
-import re  # Not really needed but most manip will involve re.
+# Most manip will involve re so we include it here for convenience.
+import re  # pylint: disable=W0611
+
+
+logger = logging.getLogger(__name__)
 
 
 class EditorError(RuntimeError):
@@ -34,11 +41,14 @@ class Editor(object):
 
     def __init__(self, **kwds):
         self.code_obj = None
-        self.code = None
+        self._code = None
+        self.dry_run = None
         if 'module' in kwds:
             self.set_module(kwds['module'])
         if 'code' in kwds:
             self.set_code_expr(kwds['code'])
+        if 'dry_run' in kwds:
+            self.dry_run = kwds['dry_run']
 
     def edit_line(self, line):
         """Edits a single line using the code expression."""
@@ -46,18 +56,19 @@ class Editor(object):
             try:
                 result = eval(self.code_obj, globals(), locals())
             except TypeError as ex:
-                raise EditorError("failed to execute %s: %s" % (self.code, ex))
-            if result is None or result is False:
+                raise EditorError("failed to execute %{0}: %{1}".format(
+                    self._code, ex))
+            if not result:
                 raise EditorError(
                         "cannot process line '%s' with %s" %
-                        (line, self.code))
+                        (line, self._code))
             elif isinstance(result, list) or isinstance(result, tuple):
-                line = ' '.join(map(str, result))
+                line = ' '.join([str(res_element) for res_element in result])
             else:
                 line = str(result)
         return line
 
-    def edit_file(self, file_name, dry_run=False):
+    def edit_file(self, file_name):
         """Edit file in place, returns a list of modifications (unified diff).
 
         Arguments:
@@ -69,15 +80,27 @@ class Editor(object):
         to_lines = [self.edit_line(line) for line in from_lines]
         diffs = difflib.unified_diff(from_lines, to_lines,
                     fromfile=file_name, tofile='<new>')
+        if not self.dry_run:
+            replace_file_name = file_name + ".new"
+            if os.path.exists(replace_file_name):
+                raise EditorError("%s already exists" % replace_file_name)
+            open(replace_file_name, "w").writelines(to_lines)
+            os.unlink(file_name)
+            os.rename(replace_file_name, file_name)
         return list(diffs)
 
     def set_code_expr(self, code):
         """Compiles the code passed as argument."""
         file_name = '<string>'
-        self.code = code
-        self.code_obj = compile(self.code, file_name, 'eval')
+        self._code = code
+        try:
+            self.code_obj = compile(self._code, file_name, 'eval')
+        except SyntaxError as error:
+            logger.error("cannot compile {0}: {1}".format(self._code, error))
+            self.code_obj = None
+            raise
 
-    def set_module(self, module):
+    def set_module(self, module):  # pylint: disable=R0201
         """Imports module that are needed for the code expr to compile.
 
         Argument:
@@ -102,22 +125,35 @@ def get_verbosity(verbose_count):
     return levels[verbose_count]
 
 
-def main(args):
+def setup_logger(verbose_count):
+    """Sets up a logger object for this script."""
+    verbosity = get_verbosity(verbose_count)
+    console = logging.StreamHandler()
+    console.setLevel(verbosity)
+    logger.addHandler(console)
+    return logger
+
+
+def command_line(argv):
     """Main command line handler."""
-    parser = argparse.ArgumentParser(description="Regex-based file editor")
-    parser.add_argument("-i", "--in-place", dest="inplace",
-            action="store_true", default=False,
-            help="in-place substitution")
+    parser = argparse.ArgumentParser(description="Python based file editor")
     parser.add_argument("-c", "--check", dest="check",
             action="store_true", default=False,
             help="only show edit that are to take place")
     parser.add_argument("-v", "--verbose", dest="verbose_count",
-            action="count", defaule="0", help="Increases verbosity")
-    args = parser.parse_args()
-    verbosity = get_verbosity(args.verbose_count)
-    logging.getLogger().setLevel(verbosity)
-    for infile in args:
-        editor.process_file(infile)
+            action="count", default="0", help="Increases verbosity")
+    parser.add_argument('-e', "--expression", dest="expression",
+            help="Python expression to be applied on all files. Use the line "
+            "variable to reference the current line.")
+    parser.add_argument('files', metavar="file", nargs='+',
+            help="file to process with the expression. Modified in place.")
+    arguments = parser.parse_args(argv)
+    setup_logger(arguments.verbose_count)
+    editor = Editor(dry_run=arguments.check)
+    if arguments.expression:
+        editor.set_code_expr(arguments.expression)
+    for infile in arguments.files:
+        editor.edit_file(infile)
     return 1
 
 
@@ -126,7 +162,7 @@ if __name__ == "__main__":
     logging.basicConfig(stream=sys.stderr)
     os_status = 1
     try:
-        main(sys.argv)
+        command_line(sys.argv)
         os_status = 0
     finally:
         logging.shutdown()
--- tests.py
+++ tests.py
@@ -5,8 +5,50 @@
 import os
 import sys
 import unittest
+import logging.config
 
 import massedit
+
+def configure_logging():
+    """Configures logging."""
+    logging_settings = {
+    'version': 1,
+    __name__: {
+        'handlers': ['file', 'console'],
+        'level': 'INFO',
+        'propagate': 1,
+        },
+    'handlers': {
+        'file': {
+        'class': 'logging.FileHandler',
+        'formatter': 'default',
+        'filename': 'estimate.log',
+        'level': 'INFO',
+        },
+        'console': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://sys.stderr',
+        'formatter': 'default',
+        'level': 'WARNING',
+        },
+        },
+    'formatters': {
+        'default': {
+        'format': 'File "%(pathname)s", line %(lineno)d, '
+                '%(name)s:%(levelname)s %(message)s',
+        }
+        }
+    }
+    logging.config.dictConfig(logging_settings)
+
+
+def get_logger():
+    configure_logging()
+    logger = logging.getLogger(__name__)
+    return logger
+
+
+logger = get_logger()
 
 
 class TestEditor(unittest.TestCase):  # pylint: disable=R0904
@@ -32,6 +74,7 @@ class TestEditor(unittest.TestCase):  # pylint: disable=R0904
         editor = massedit.Editor()
         with self.assertRaises(SyntaxError):
             editor.set_code_expr("invalid expression")
+            self.assertIsNone(self.code_obj)
 
     def test_invalid_code_expr2(self):
         """Checks we get a SyntaxError if the code is not valid."""
@@ -71,7 +114,7 @@ class TestEditorWithFile(unittest.TestCase):  # pylint: disable=R0904
     def setUp(self):
         """Creates a temporary file to work with."""
         self.file = open("test_file.txt", "w")
-        self.file.write("""The Zen of Python, by Tim Peters
+        self.text = """The Zen of Python, by Tim Peters
 
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -92,7 +135,8 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
-""")
+"""
+        self.file.write(self.text)
         self.file.close()
 
     def tearDown(self):
@@ -107,7 +151,7 @@ Namespaces are one honking great idea -- let's do more of those!
         """Checks editing of an entire file."""
         editor = massedit.Editor()
         editor.set_code_expr("re.sub('Dutch', 'Guido', line)")
-        diffs = editor.edit_file(self.file.name, dry_run=True)
+        diffs = editor.edit_file(self.file.name)
         self.assertEquals(len(diffs), 11)
         expected_first_diff = """\
  There should be one-- and preferably only one --obvious way to do it.
@@ -116,6 +160,30 @@ Namespaces are one honking great idea -- let's do more of those!
  Now is better than never.
 """
         self.assertEquals("".join(diffs[5:9]), expected_first_diff)
+
+    def test_command_line_replace(self):
+        """Checks simple replacement via command line."""
+        massedit.command_line(["-e","re.sub('Dutch', 'Guido', line)",
+            self.file.name])
+        new_lines = open(self.file.name, "r").readlines()
+        original_lines = self.text.splitlines(True)
+        length = max(len(new_lines), len(original_lines))
+        for index in xrange(length):
+            line = index + 1
+            if line != 16:
+                self.assertEquals(new_lines[index], original_lines[index])
+            else:
+                expected_line_16 = "Although that way may not be obvious " + \
+                        "at first unless you're Guido.\n"
+                self.assertEquals(new_lines[index], expected_line_16)
+
+    def test_command_line_check(self):
+        """Checks simple replacement via command line."""
+        massedit.command_line(["-e","re.sub('Dutch', 'Guido', line)",
+            self.file.name, "--check"])
+        new_lines = open(self.file.name, "r").readlines()
+        original_lines = self.text.splitlines(True)
+        self.assertEquals(original_lines, new_lines)
 
 
 if __name__ == "__main__":
