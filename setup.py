4689579301728d8acec7d8f086073df5b362ab88
elmotec <jlecomte1972@yahoo.com>
2012-03-18 21:09:27 -0400

    Renamed project

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "massedit",
+    name = "Python Mass Editor",
     version = "0.1",
     author = "Jerome Lecomte",
     author_email = "jlecomte1972@yahoo.com",
