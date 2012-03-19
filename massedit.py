4b78aeddccd046e29795292353c1921478884b12
elmotec <jlecomte1972@yahoo.com>
2012-03-18 21:09:06 -0400

    removed doc redundant with README

--- massedit.py
+++ massedit.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 
-"""Implements a python stream editor class to process lines or files.
+"""Implements a python mass editor class to apply the same code to many files.
 
-I have been using runsed and checksed (from Unix Power Tools) for years and
-did not find a good substitute under Windows until I came accross Graham
-Fawcett python recipe on :
-http://code.activestate.com/recipes/\
-        437932-pyline-a-grep-like-sed-like-command-line-tool/
-
-The core was fleshed up a little, and here we are. If you find it usefull and
-enhance it please, do not forget to submit patches. Thanks!
+See README.rst for more information.
 
 Licensed under the term of MIT License. See file LICENSE.
 """
 
-__version__ = '0.1'
+__version__ = '0.1'  # Update setup.py when changing version.
+
 
 import os
 import logging
