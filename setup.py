933913bf16639e2c66c298060d55246b07da12f0
elmotec <elmotec@gmx.com>
2019-04-13 09:07:08 -0400

    bumped version on setup.py as well

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ readme = open(os.path.join(here, 'README.rst')).read()
 
 setup(
     name="massedit",
-    version="0.68.4",
+    version="0.68.5",
     author="Jérôme Lecomte",
     author_email="elmotec@gmx.com",
     description='Edit multiple files using Python text processing modules',
